83597a320efac395e8f8355286b565b179b5a342
Subash Pradahan <suprowilde5@gmail.com>
2019-09-13 16:27:27 +0200

    Starting project with material-ui, test check if material ui is working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
+import Navigator from './Components/Navigator'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <Navigator />
+      <Button variant="content" color="red">
       Hello
+      </Button>
     </div>
   );
 }
