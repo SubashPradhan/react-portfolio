72391b18b310b80974fdcb45c1cc7ad2142dac81
Subash Pradahan <suprowilde5@gmail.com>
2019-09-16 12:21:55 +0200

    Wroking on background and slider

--- src/App.js
+++ src/App.js
@@ -2,30 +2,29 @@ import React, { Component } from 'react';
 import Home from './Components/Home'
 import Navigator from './Components/Home/Navigator'
 import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Background from './Components/Home/Background';
 
 class App extends Component {
 
   render() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 0,
-    // slidesToScroll: 1
-  };
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1  
+    };
 
-
-    return <div> 
-      {/* <Navigator /> */}
+    return <div>
+      <Navigator />
+      <Background />
       <Slider {...settings}>
-      
-
-          <Home />
-      
-      <div>
-
-          <img src = "https://inchoo.net/wp-content/uploads/2017/08/Slick_Carousel.png" alt='hey'/>
-      </div>
+        <Home />
+        <div>
+          <img src="https://inchoo.net/wp-content/uploads/2017/08/Slick_Carousel.png" alt='hey'/>
+        </div>
       </Slider>
     </div>
 
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -3,18 +3,9 @@ import React, { Component } from 'react'
 import View from './view'
 
 class Home extends Component {
-  state = { background: '' }
-  
-  componentDidMount() {
-    setTimeout(function () {
-      this.setState({
-        background: 'true'
-      })
-    }.bind(this), 7000)
-  }
   render() {
     return <div>
-      <View background={this.state.background} />
+      <View />
     </div>
 
   }
