390bf7acabc4eb63e2ba6d3305459a5a2af3a9ff
Subash Pradahan <suprowilde5@gmail.com>
2020-12-02 13:38:06 +0100

    Change my picture

--- src/Components/Home/view.js
+++ src/Components/Home/view.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import '../Style/Home.css'
 import Typist from 'react-typist'
-
+import Subash from '../images/subash.jpg'
 
 export default function View() {
   return <div align='center'>
-    <img src='Subash.JPG' alt='Subash' className='my-picture' />
+    <img src={Subash} alt='Subash' className='my-picture' />
     <Typist cursor={{show: false}}>
       <div className='welcome-text'>
         Name: Subash Pradhan<br />
