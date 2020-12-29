3b2fe681a10e646c0d92a7ad7f5eb8e20d245c70
Subash Pradahan <suprowilde5@gmail.com>
2020-12-29 15:47:45 +0100

    Add symbols for headers

--- src/Components/Skills/view.js
+++ src/Components/Skills/view.js
@@ -1,21 +1,58 @@
 import React from 'react';
-import '../Style/Skill.css';
-import SkillBar from 'react-skillbars';
+import JSIcon from '../../assets/JS.svg';
+import ReactIcon from '../../assets/react.svg';
+import VueIcon from '../../assets/vue.svg';
+import HTML5Icon from '../../assets/html5.svg';
+import Css3Icon from '../../assets/css3.svg';
+import NodeIcon from '../../assets/nodejs.svg';
 import Tada from 'react-reveal/Tada';
+import '../Style/Skill.css';
 
-export default function View(props) {
-	const { skills, skillColors } = props;
+export default function View() {
 	return (
-		<div align="center">
+		<div className="skills-container">
 			<Tada>
-				<h1>Skills</h1>
+				<h1>{'[ Skills ]'}</h1>
 			</Tada>
-			<div className="skills" align="center">
-				<SkillBar
-					skills={skills}
-					colors={skillColors}
-					animationDuration={2000}
-				/>
+			<div className="skills-collections">
+				<div className="skills">
+					<img
+						src={HTML5Icon}
+						className="skill-icons"
+						title="HTML5"
+						alt="HTML5"
+					/>
+				</div>
+				<div className="skills">
+					<img src={Css3Icon} className="skill-icons" title="Css3" alt="Css3" />
+				</div>
+				<div className="skills">
+					<img
+						src={JSIcon}
+						className="skill-icons"
+						title="Javascript"
+						alt="Javascript"
+					/>
+				</div>
+				<div className="skills">
+					<img
+						src={ReactIcon}
+						className="skill-icons"
+						title="React"
+						alt="React"
+					/>
+				</div>
+				<div className="skills">
+					<img src={VueIcon} className="skill-icons" title="Vue" alt="Vue" />
+				</div>
+				<div className="skills">
+					<img
+						src={NodeIcon}
+						className="skill-icons"
+						title="Nodejs"
+						alt="Nodejs"
+					/>
+				</div>
 			</div>
 		</div>
 	);
