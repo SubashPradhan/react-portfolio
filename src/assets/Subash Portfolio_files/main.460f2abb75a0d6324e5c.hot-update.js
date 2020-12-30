56a5d763e389f2b03b3725efef0e8994dd285fa0
Subash Pradahan <suprowilde5@gmail.com>
2020-12-30 06:08:54 +0100

    Update webpacks

---
+++ src/assets/Subash Portfolio_files/main.460f2abb75a0d6324e5c.hot-update.js
@@ -0,0 +1,19 @@
+webpackHotUpdate("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./src/Style/About.css":
+/*!********************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ref--6-oneOf-3-1!./node_modules/postcss-loader/src??postcss!./src/Style/About.css ***!
+  \********************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js")(false);
+// Module
+exports.push([module.i, ".vertical-timeline-element {\n\tcolor: blue;\n\tfont-size: 1.3rem;\n\tletter-spacing: 0.15rem;\n}\n\n.vertical-timeline-element-title {\n\tfont-size: 1.3rem;\n}\n\n.vertical-timeline-element-date {\n\tcolor: white;\n\tfont-weight: 900;\n\tfont-size: 2.5rem;\n}\n\n.vertical-timeline-element p {\n\tfont-weight: bold;\n\tcolor: black;\n}\n\n@media only screen and (max-width: 700px) {\n\t.vertical-timeline-element-date {\n\t\tcolor: black;\n\t}\n}\n", ""]);
+
+
+
+/***/ })
+
+})
+//# sourceMappingURL=main.460f2abb75a0d6324e5c.hot-update.js.map
---
+++ src/assets/Subash Portfolio_files/main.5e7fd7c6d896569196a1.hot-update.js
@@ -0,0 +1,19 @@
+webpackHotUpdate("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./src/Style/About.css":
+/*!********************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ref--6-oneOf-3-1!./node_modules/postcss-loader/src??postcss!./src/Style/About.css ***!
+  \********************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js")(false);
+// Module
+exports.push([module.i, ".vertical-timeline-element {\n\tcolor: blue;\n\tfont-size: 1.3rem;\n\tletter-spacing: 0.15rem;\n\tfont-size: 1rem;\n}\n\n.vertical-timeline-element-date {\n\tcolor: white;\n\tfont-weight: 900;\n}\n\n.vertical-timeline-element p {\n\tfont-weight: bold;\n\tcolor: black;\n\t/* font-size: 0.9rem; */\n}\n\n@media only screen and (max-width: 700px) {\n\t.vertical-timeline-element-date {\n\t\tcolor: black;\n\t}\n}\n", ""]);
+
+
+
+/***/ })
+
+})
+//# sourceMappingURL=main.5e7fd7c6d896569196a1.hot-update.js.map
